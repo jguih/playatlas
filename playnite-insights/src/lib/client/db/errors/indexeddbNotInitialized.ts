99c86b5d53a7484e7adff9dacc03ab2c760364a3
Jose Guilherme <gui.kcoal+github@hotmail.com>
2026-01-19 20:27:10 -0300

    chore: remove old components

--- playnite-insights/src/lib/client/db/errors/indexeddbNotInitialized.ts
+++
@@ -1,6 +0,0 @@
-export class IndexedDBNotInitializedError extends Error {
-	constructor() {
-		super("IndexedDB is not initialized");
-		this.name = "IndexedDBNotInitializedError";
-	}
-}
--- playnite-insights/src/routes/+page.svelte
+++ playnite-insights/src/routes/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import Dashboard from "$lib/client/components/bottom-nav/Dashboard.svelte";
-	import Home from "$lib/client/components/bottom-nav/Home.svelte";
-	import Settings from "$lib/client/components/bottom-nav/Settings.svelte";
-	import BottomNav from "$lib/client/components/BottomNav.svelte";
-	import Header from "$lib/client/components/header/Header.svelte";
-	import BaseAppLayout from "$lib/client/components/layout/BaseAppLayout.svelte";
-	import Main from "$lib/client/components/Main.svelte";
+	import Dashboard from "$lib/ui/components/bottom-nav/Dashboard.svelte";
+	import Home from "$lib/ui/components/bottom-nav/Home.svelte";
+	import Settings from "$lib/ui/components/bottom-nav/Settings.svelte";
+	import BottomNav from "$lib/ui/components/BottomNav.svelte";
+	import Header from "$lib/ui/components/header/Header.svelte";
+	import BaseAppLayout from "$lib/ui/components/layout/BaseAppLayout.svelte";
+	import Main from "$lib/ui/components/Main.svelte";
 </script>
 
 <BaseAppLayout>
 	<Header></Header>
 	<Main></Main>
-
 	<BottomNav>
 		<Home selected={true} />
 		<Dashboard />
