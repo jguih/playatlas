9a8987cedfef4b58cf721dd4ce3435d2714388f8
Jose Guilherme <gui.kcoal+github@hotmail.com>
2026-01-20 17:01:53 -0300

    chore: fixed backdrop accessibility warning

--- playnite-insights/src/lib/ui/components/sidebar/Backdrop.svelte
+++ playnite-insights/src/lib/ui/components/sidebar/Backdrop.svelte
@@ -8,9 +8,7 @@
 
 <div
 	{...props}
-	aria-hidden="true"
 	role="presentation"
-	tabindex="-1"
 	class={["fixed right-0 top-0 z-40 h-full w-full max-w-full bg-black opacity-50", props.class]}
 	transition:fade={{ duration: 150, easing: cubicOut }}
 ></div>
